277890032d667938bc6aa8de9878fbfc2296ab0c
v1rtl <pilll.PL22@gmail.com>
2020-06-30 01:36:47 +0300

    format tests

--- __tests__/express.test.ts
+++ __tests__/express.test.ts
@@ -3,12 +3,12 @@ import supertest from 'supertest'
 import { json, form, ReqWithBody } from '../src/index'
 
 describe('Express middleware test', () => {
-  it('should parse JSON body', (done) => {
+  it('should parse JSON body', done => {
     const app = Express()
 
     app.use(json())
 
-    app.post('/', async (req: ReqWithBody, res, next) => {
+    app.post('/', async (req: ReqWithBody, res) => {
       res.setHeader('Content-Type', 'application/json')
       res.json(req.body)
     })
@@ -22,20 +22,20 @@ describe('Express middleware test', () => {
       .send({ hello: 'world' })
       .set('Accept', 'application/json')
       .expect(200, {
-        hello: 'world',
+        hello: 'world'
       })
-      .end((err) => {
+      .end(err => {
         server.close()
         if (err) return done(err)
         done()
       })
   })
-  it('should parse form', (done) => {
+  it('should parse form', done => {
     const app = Express()
 
     app.use(form())
 
-    app.post('/', async (req: ReqWithBody, res, next) => {
+    app.post('/', async (req: ReqWithBody, res) => {
       res.setHeader('Content-Type', 'application/json')
       res.json(req.body)
     })
@@ -49,9 +49,9 @@ describe('Express middleware test', () => {
       .send('hello=world')
       .set('Accept', 'application/json')
       .expect(200, {
-        hello: 'world',
+        hello: 'world'
       })
-      .end((err) => {
+      .end(err => {
         server.close()
         if (err) return done(err)
         done()
