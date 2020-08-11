0bfe17a577ca12b05b12333b16ef634118b93c6a
v1rtl <pilll.pl22@gmail.com>
2020-08-11 21:39:49 +0300

    Replace rollup with tsup, improve type passing, fails to build

--- src/index.ts
+++ src/index.ts
@@ -1,38 +1,43 @@
-import { IncomingMessage as Request, ServerResponse as Response } from 'http'
+import { ServerResponse as Response } from 'http'
 import * as qs from 'querystring'
-import { once } from 'events'
+import { once, EventEmitter } from 'events'
 
 // Extend the request object with body
-export type ReqWithBody = Request & {
+export type ReqWithBody = EventEmitter & {
   body?: any
 }
 
 // Main function
-const parsec = (fn: (body: any) => void, T?: any) => async (req: ReqWithBody | typeof T, _res: Response, next?: () => void) => {
+const parsec = <T extends ReqWithBody>(fn: (body: any) => void) => async (req: ReqWithBody | T, _res: Response) => {
   const body = await once(req, 'data').then((data) => {
     return fn(data)
   })
+
   req.body = body
 
-  next?.()
+  await once(req, 'end')
 }
 
 // JSON, raw, FormData
 
 const json = () => async (req: ReqWithBody, _res?: Response, next?: () => void) => {
-  await parsec((x) => JSON.parse(x.toString()))(req, _res, next)
+  await parsec((x) => JSON.parse(x.toString()))(req, _res)
+  next?.()
 }
 
 const raw = () => async (req: ReqWithBody, _res?: Response, next?: () => void) => {
-  await parsec((x) => x)(req, _res, next)
+  await parsec((x) => x)(req, _res)
+  next?.()
 }
 
 const text = () => async (req: ReqWithBody, _res?: Response, next?: () => void) => {
-  await parsec((x) => x.toString())(req, _res, next)
+  await parsec((x) => x.toString())(req, _res)
+  next?.()
 }
 
 const form = () => async (req: ReqWithBody, _res?: Response, next?: () => void) => {
-  await parsec((x) => qs.parse(x.toString()))(req, _res, next)
+  await parsec((x) => qs.parse(x.toString()))(req, _res)
+  next?.()
 }
 
 export { parsec as custom, json, raw, text, form }
--- src/koa.ts
+++ src/koa.ts
@@ -1,12 +1,6 @@
 import * as parsec from './index'
-import {
-  Response,
-  Next,
-  ParameterizedContext,
-  DefaultState,
-  DefaultContext,
-} from 'koa'
-import { IncomingMessage, ServerResponse } from 'http'
+import { Next, ParameterizedContext, DefaultState, DefaultContext } from 'koa'
+import { IncomingMessage } from 'http'
 import * as qs from 'querystring'
 
 export type CtxWithBody = ParameterizedContext<DefaultState, DefaultContext> & {
@@ -15,24 +9,27 @@ export type CtxWithBody = ParameterizedContext<DefaultState, DefaultContext> & {
   }
 }
 
-const custom = (fn = (body: any) => body) => (ctx: CtxWithBody, next?: Next) => {
-  parsec.custom(fn, Request)(ctx.req, ctx.res, next)
+const custom = (fn = (body: any) => body) => async (ctx: CtxWithBody, next?: Next) => {
+  await parsec.custom(fn)(ctx.req, ctx.res)
+  next?.()
 }
 
-const json = () => (ctx: CtxWithBody, next?: Next) => {
-  return parsec.custom((x) => JSON.parse(x.toString()))(ctx.req, ctx.res, next)
+const json = () => async (ctx: CtxWithBody, next?: Next) => {
+  await custom((x) => JSON.parse(x.toString()))(ctx)
+  next?.()
 }
 
-const raw = () => (ctx: CtxWithBody, next?: Next) => {
-  return parsec.custom((x) => x)(ctx.req, ctx.res, next)
+const raw = () => async (ctx: CtxWithBody, next?: Next) => {
+  await custom((x) => x)(ctx)
+  next?.()
 }
 
 const text = () => (ctx: CtxWithBody, next?: Next) => {
-  return parsec.custom((x) => x.toString())(ctx.req, ctx.res, next)
+  return custom((x) => x.toString())(ctx)
 }
 
 const form = () => (ctx: CtxWithBody, next?: Next) => {
-  return parsec.custom((x) => qs.parse(x.toString()))(ctx.req, ctx.res, next)
+  return custom((x) => qs.parse(x.toString()))(ctx)
 }
 
 export { custom, json, raw, text, form }
